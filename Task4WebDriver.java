85486993b524dbb5ce3faceec869a0054595c430
Liliia88 <khakimova.liliya.ravkatovna@gmail.com>
2022-10-24 14:02:12 -0400

    d

--- Task4WebDriver.java
+++ Task4WebDriver.java
@@ -1,5 +1,4 @@
 package GroupProject2;
-
 public interface Task4WebDriver {
      /*Provide Implementation for the diagram below. Then create a test class in which you need to create Objects of
  ChromeDriver, FirefoxDrive and SafariDriver classes and see which methods available to them.*/
@@ -30,7 +29,6 @@ class ChromeDriver implements RemoteWebdriver{
         System.out.println("get screenshot of Chrome web browser");
     }
 }
-
 class FirefoxDriver implements RemoteWebdriver{
     public void open(){
         System.out.println("open a Firefox driver");
@@ -65,36 +63,14 @@ class FirefoxDriver implements RemoteWebdriver{
          System.out.println("get screenshot of Firefox web browser");
      }
  }
-
  class tester30 {
      public static void main(String[] args) {
-         /*ChromeDriver chrome=new ChromeDriver();
-         chrome.open();
-         chrome.close();
-         System.out.println(chrome.getTitle());
-         chrome.navigate();
-         chrome.getScreenshot();
-         FirefoxDriver firefox=new FirefoxDriver();
-         firefox.open();
-         firefox.close();
-         System.out.println(firefox.getTitle());
-         firefox.navigate();
-         firefox.getScreenshot();
-         SafariDriver safari=new SafariDriver();
-         safari.open();
-         safari.close();
-         System.out.println(safari.getTitle());
-         safari.navigate();
-         safari.getScreenshot();*/
-
          RemoteWebdriver[] remote={new ChromeDriver(),new FirefoxDriver(),new SafariDriver()};
          for (RemoteWebdriver r:remote){
              r.open();
              r.close();
              System.out.println(r.getTitle());
              r.navigate();
-
-             //comment
          }
      }
  }
